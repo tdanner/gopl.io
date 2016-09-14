b824293008b2362f822bcd17fc5a5cd2697f4ed9
Tim Danner <tdanner@gmail.com>
2016-09-13 22:30:59 -0500

    Green on black!

--- ch1/lissajous/main.go
+++ ch1/lissajous/main.go
@@ -28,7 +28,7 @@ import (
 
 //!+main
 
-var palette = []color.Color{color.White, color.Black}
+var palette = []color.Color{color.Black, color.RGBA{0x00, 0xFF, 0x00, 0xFF}}
 
 const (
 	whiteIndex = 0 // first color in palette
