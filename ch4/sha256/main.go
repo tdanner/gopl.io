3091970473d8129cf94d4367c34fee03158aa64f
Tim Danner <tdanner@gmail.com>
2016-09-24 17:54:47 -0500

    Command line option for sha256/384/512

--- ch4/sha256/main.go
+++ ch4/sha256/main.go
@@ -12,12 +12,30 @@ import (
 	"io"
 	"log"
 	"crypto/sha256"
+	"crypto/sha512"
+	"hash"
 )
 
 //!+
 
 func main() {
-	hash := sha256.New()
+	var hash hash.Hash
+	if len(os.Args) == 2 {
+		if os.Args[1] == "sha384" {
+			hash = sha512.New384()
+		} else if os.Args[1] == "sha512" {
+			hash = sha512.New()
+		} else if os.Args[1] == "sha256" {
+			hash = sha256.New()
+		} else {
+			log.Fatal(fmt.Sprintf("Unknown hash '%s'", os.Args[1]))
+		}
+	}
+
+	if hash == nil {
+		hash = sha256.New()
+	}
+
 	n, err := io.Copy(hash, os.Stdin)
 	if err != nil {
 		log.Fatal(err)
