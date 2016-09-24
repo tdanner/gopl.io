ca0d6c3a4c0a807b9d96a6c5bfebfe3827f0e344
Tim Danner <tdanner@gmail.com>
2016-09-24 17:43:51 -0500

    Hash stdin

--- ch4/sha256/main.go
+++ ch4/sha256/main.go
@@ -6,20 +6,23 @@
 // The sha256 command computes the SHA256 hash (an array) of a string.
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"io"
+	"log"
+	"crypto/sha256"
+)
 
 //!+
-import "crypto/sha256"
 
 func main() {
-	c1 := sha256.Sum256([]byte("x"))
-	c2 := sha256.Sum256([]byte("X"))
-	fmt.Printf("%x\n%x\n%t\n%T\n", c1, c2, c1 == c2, c1)
-	// Output:
-	// 2d711642b726b04401627ca9fbac32f5c8530fb1903cc4db02258717921a4881
-	// 4b68ab3847feda7d6c62c1fbcbeebfa35eab7351ed5e78f4ddadea5df64b8015
-	// false
-	// [32]uint8
+	hash := sha256.New()
+	n, err := io.Copy(hash, os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%d bytes hashed. Hash = \n%x\n", n, hash.Sum(nil))
 }
 
 //!-
