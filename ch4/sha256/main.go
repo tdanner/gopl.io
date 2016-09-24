0430646dec66b64c087f1e0b74eec58e58097eab
Tim Danner <tdanner@gmail.com>
2016-09-24 17:26:35 -0500

    Print number of different bits

--- ch4/sha256/main.go
+++ ch4/sha256/main.go
@@ -13,13 +13,32 @@ import "crypto/sha256"
 
 func main() {
 	c1 := sha256.Sum256([]byte("x"))
-	c2 := sha256.Sum256([]byte("X"))
-	fmt.Printf("%x\n%x\n%t\n%T\n", c1, c2, c1 == c2, c1)
+	c2 := sha256.Sum256([]byte("X12345"))
+	fmt.Printf("%x\n%x\n%t\n%T\n%d\n", c1, c2, c1 == c2, c1, numberOfDifferentBitsSha(c1, c2))
 	// Output:
 	// 2d711642b726b04401627ca9fbac32f5c8530fb1903cc4db02258717921a4881
 	// 4b68ab3847feda7d6c62c1fbcbeebfa35eab7351ed5e78f4ddadea5df64b8015
 	// false
 	// [32]uint8
+}
+
+func numberOfDifferentBitsSha(a, b [32]byte) int {
+	n := 0
+	for i := range a {
+		n += numberOfDifferentBits(a[i], b[i])
+	}
+	return n
+}
+
+
+func numberOfDifferentBits(a, b byte) int {
+	var n byte
+	for i := 0; i < 8; i++ {
+		n += a&1 ^ b&1
+		a >>= 1
+		b >>= 1
+	}
+	return int(n)
 }
 
 //!-
